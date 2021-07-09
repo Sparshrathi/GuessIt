2b5ace7faff7bf317b744e57f0a4f63c7d1ce1d0
Sparsh Rathi <sparshrathi1999@gmail.com>
2021-07-09 16:12:43 +0530

    add Alert box, control Soft Ketboard , Handling user Input

--- screen/StartGameScreen.js
+++ screen/StartGameScreen.js
@@ -1,28 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View,TextInput ,Button} from 'react-native';
+import React ,{useState} from 'react';
+import { StyleSheet, Alert, Text, View,TouchableWithoutFeedback,Button,Keyboard} from 'react-native';
 import Colour from './Constants/Colour';
 import Card from '../Components/Card ';
 import Input from '../Components/Input';
 
 const StartGameScreen = props => {
+
+    const [enterValue,setEnterValue]= useState('');
+    const [confirmed, setConfirmed] = useState(false)
+    const [seletedValue,setSeletedValue] = useState();
+    const numberInputHandler= (inputText) =>{
+
+        setEnterValue(inputText.replace(/[^0-9]/g,''));
+    }
+
+    const resetInputHandler= () =>{
+        setEnterValue('');
+        setConfirmed(false)
+    }
+
+    const confirmInputHandler=()=>{
+            const chosenNumber = parseInt(enterValue);
+            if(isNaN(chosenNumber) || chosenNumber > 99 || chosenNumber <=0)
+            {
+                Alert.alert('Invalid Number','Number has to br between 1 to 99',
+                [{text:'Okay' , style:'destructive',onPress: resetInputHandler }])
+                return;
+            } 
+        setConfirmed(true);
+        setSeletedValue(chosenNumber);
+        setEnterValue('');
+        }
+
+        let confirmedOutput; 
+        if(confirmed){
+            confirmedOutput= <Text>Chosen Number : {seletedValue} </Text>
+        }
+
     return (
-        <View style={styles.screen}>
+        <TouchableWithoutFeedback 
+        onPress={() =>{Keyboard.dismiss();}}
+        > 
+            <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game !!</Text>
-           <Card style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <Input style={styles.input}autoCapitalize="none" autoCorrect={false} keyboardType="numeric" blurOnSubmit maxLength={2} />
+                <Input style={styles.input} 
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  keyboardType="number-pad"
+                  blurOnSubmit 
+                  maxLength={2} 
+                  onChangeText={numberInputHandler}
+                  value={enterValue}
+                  />
                 <View style={styles.buttonContainer}>
-                    <View style={styles.button}>   
-                        <Button title={'Reset'} onPress={() =>{}} color={Colour.accent}  />
+                <View style={styles.button}>   
+                <Button title={'Reset'} onPress={resetInputHandler} color={Colour.accent}  />
                     </View>
                     
                     <View style={styles.button}>
-                      <Button title={'Confirm'} onPress={() =>{}} color={Colour.primary} />
+                      <Button title={'Confirm'} onPress={confirmInputHandler} color={Colour.primary} />
                       </View>
-                </View>
+                      </View>
             </Card>
-        </View>
-    )
+            {confirmedOutput}
+            </View>
+        </TouchableWithoutFeedback>
+            )
 }
 
 const styles = StyleSheet.create({
